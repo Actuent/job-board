f26df48bd45b3589001ee39034a6ae6a1e51a226
Orton Babb <ortonbabb@gmail.com>
2021-02-15 21:18:07 -0500

    Search functionality implementation with some page updates
    Search is now functional with the basic regex filter (not so accurate), updated pages, updated virtual environment

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import json
+from jobfilter import JobFilter
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-   return render_template('index.html')
+    #This if statement handles the search requests both from index and the search page
+    if request.method == 'POST':
+        jobtitle = request.form.get('jobtitle')
+        location = request.form.get('location')
+        return redirect(url_for('search', jobtitle=jobtitle, location=location))
+    return render_template('index.html')
 
 @app.route('/about/')
 def about():
@@ -21,9 +30,40 @@ def privacy():
 def credits():
     return render_template('credits.html')
 
-@app.route('/search/')
+@app.route('/search')
 def search():
-    return render_template('search.html')
+    if request.args:
+        # We have our query string nicely serialized as a Python dictionary
+        args = request.args
+
+        if "jobtitle" in args:
+            jobtitle = args["jobtitle"]
+        else:
+            jobtitle = ""
+
+        if "location" in args:
+            location = args["location"]
+        else:
+            location = ""
+
+
+    else:
+        #Set defaults for search page
+        jobtitle = "driver"
+        location = "washington+dc"
+
+    # Need to include end user ip and end user's "useragent"
+    talent = json.loads(requests.get(f'https://neuvoo.com/services/api-new/search?ip=1.1.1.1&useragent=123asd&k={jobtitle}&l={location}&contenttype=all&format=json&publisher=92f7a67c&cpcfloor=1&subid=10101&jobdesc=1&country=us&radius=50').text)
+
+    # Filter the dictionary
+    filtered_jobs = JobFilter(talent['results'])
+    numjobs = len(filtered_jobs)
+
+    # Create a description preview for the html rendering
+    for job in filtered_jobs:
+        job['description_preview'] = job['description'][0:180] + "..."
+
+    return render_template('search.html', jobs=filtered_jobs, numjobs=numjobs, jobtitle=jobtitle, location=location)
 
 @app.route('/howitworks/')
 def howitworks():
