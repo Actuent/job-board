30f22642a931bd51c3aa8e17407cb60655893db7
Orton Babb <ortonbabb@gmail.com>
2021-02-20 16:05:01 -0500

    Temporarily refactor code to avoid relative imports

--- app/app.py
+++ app/app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
-from jobfilter import JobFilter
+import re
+
+def isEntryLevelQ(job_title, job_description):
+
+    # Define the patterns
+    title_pattern = r"I{2,3}|IV|[sS]enior|[sS]r\.*|[lL]ead|[dD]irector|[Pp]rincipal|[mM]anage|[eE]xperienced"
+
+    description_pattern = r"(?:[mM]inimum|[mM]in\.?|[aA]t least|[tT]otal|[iI]ncluding|[wW]ith).+(?:\d+|one|two|three|four|five|six|seven|eight|nine|ten)\+* (?:[yY]ears*|[yY]rs*)|[mM]aster'?s"
+
+    # Try to find the patterns
+    inTitleQ = re.search(title_pattern, job_title) != None
+    inDescriptionQ = re.search(description_pattern, job_description) != None
+
+    # Consider entry level if no matches
+    return not (inTitleQ or inDescriptionQ)
+
+def JobFilter(job_list):
+    return [job for job in job_list if isEntryLevelQ(job['jobtitle'], job['description'])]
 
 app = Flask(__name__)
 
