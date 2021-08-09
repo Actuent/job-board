1f788bda1e83ac47e7cad7099a14c8f358fcf935
AndrewHornstra <andrewhornstra@gmail.com>
2021-08-08 22:39:12 -0400

    Turn off debug

--- app/app.py
+++ app/app.py
@@ -181,4 +181,4 @@ def page_not_found(e):
     return render_template('404.html', error=e), 404
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, debug=True)
+    app.run(use_reloader=True, debug=False)
